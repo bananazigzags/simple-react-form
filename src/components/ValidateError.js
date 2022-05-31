21738a328e1148a715cf7b0142cf67f5e49d6954
bananazigzags <bananazigzags@gmail.com>
2022-05-31 15:34:29 +0300

    fixed className

--- src/components/ValidateError.js
+++ src/components/ValidateError.js
@@ -3,6 +3,6 @@ import "./styles/ValidateError.css"
 
 export default class ValidateError extends React.Component {
   render () {
-    return !this.props.isValid ? <span class="error">{this.props.message}</span> : null
+    return !this.props.isValid ? <span className="error">{this.props.message}</span> : null
   }
 }
